243265ef01d74f274efdf9fe24cd25f2dd1d8982
Yasu31 <yasu313nori@gmail.com>
2018-04-01 23:02:28 +0900

    feat: aligns z axis of target frame with that of outside frame (this should be re-implemented in a better way...)

--- src/ikpy/inverse_kinematics.py
+++ src/ikpy/inverse_kinematics.py
@@ -23,7 +23,11 @@ def inverse_kinematic_optimization(chain, target_frame, starting_nodes_angles, r
     def optimize_target(x):
         # y = np.append(starting_nodes_angles[:chain.first_active_joint], x)
         y = chain.active_to_full(x, starting_nodes_angles)
-        squared_distance = np.linalg.norm(chain.forward_kinematics(y)[:3, -1] - target)
+        fk=chain.forward_kinematics(y)
+
+        squared_distance = np.linalg.norm(fk[:3, -1] - target) + 0.025*np.linalg.norm(fk[2,:3]-target_frame[2,:3])
+        # the second part is the cost function to keep the z axis of the target frame aligned with the world's z axis. The coefficient was decided very experimentally- change it as much as 0.1 and it won't work... should find better solution.
+
         return squared_distance
 
     # If a regularization is selected
@@ -44,6 +48,15 @@ def inverse_kinematic_optimization(chain, target_frame, starting_nodes_angles, r
     # Manage iterations maximum
     if max_iter is not None:
         options["maxiter"] = max_iter
+
+        # Add Constraints
+    cons = ({'type':'eq','fun': lambda x: x[1] + x[2] + x[4] + x[5]},
+            {'type':'eq','fun': lambda x: x[3]}
+            )
+
+    # Utilisation d'une optimisation SLSQP_**
+    # https://forum.poppy-project.org/t/inverse-kinematic-with-pen-holder-poppy-ergo-jr/2601/7
+    # res = scipy.optimize.minimize(optimize_total, chain.active_from_full(starting_nodes_angles), method='SLSQP', bounds=real_bounds, options=options, constraints=cons)
 
     # Utilisation d'une optimisation L-BFGS-B
     res = scipy.optimize.minimize(optimize_total, chain.active_from_full(starting_nodes_angles), method='L-BFGS-B', bounds=real_bounds, options=options)
